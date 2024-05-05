16a044678d13291537c8e7b973f30f905d152fdb
fucloud-macbook <270928474@qq.com>
2024-05-05 10:19:04 +0800

    fix: can't find vue module

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,4 @@
 /// <reference types="vite/client" />
+
+// https://stackoverflow.com/questions/54839057/vscode-showing-cannot-find-module-ts-error-for-vue-import-while-compiling-doe
+declare module "*.vue";

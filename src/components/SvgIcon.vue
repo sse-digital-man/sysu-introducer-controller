e989ca57c1d8c46aa23b4abbe2142cdfde92e86d
fucloud-macbook <270928474@qq.com>
2024-05-05 18:22:27 +0800

    feat: import svg icon

---
+++ src/components/SvgIcon.vue
@@ -0,0 +1,28 @@
+<template>
+    <!-- 通过变量设置属性 -->
+
+    <el-icon :style="{ 'font-size': size + 'px' }">
+        <Icon :style="{ fill: color }"></Icon>
+    </el-icon>
+</template>
+
+<script setup lang="ts">
+import { defineAsyncComponent } from "vue";
+
+const props = defineProps({
+    name: {
+        type: String,
+        required: true,
+    },
+    size: {
+        type: Number,
+        default: 32,
+    },
+    color: {
+        type: String,
+        default: "black",
+    },
+});
+
+const Icon = defineAsyncComponent(() => import(`../assets/icons/${props.name}.svg`));
+</script>

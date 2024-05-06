df4d45c19ecca314a5f941f57337315eb93eed67
fucloud-macbook <270928474@qq.com>
2024-05-06 14:51:54 +0800

    feat: basic structure for controlling module

---
+++ src/components/ModuleControlCell.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="wrap">
+        <div>
+            <span id="title"> {{ title }} </span>
+            <el-divider />
+            <div id="status">状态: {{ statusLabel[status] }}</div>
+        </div>
+        <div>
+            <el-button style="float: right" color="var(--el-color-info-light-9)" circle>
+                <SvgIcon name="play" :size="24" color="green" />
+            </el-button>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import SvgIcon from "./SvgIcon.vue";
+
+import { statusLabel } from "../info/status";
+
+export default {
+    components: { SvgIcon },
+    props: {
+        title: {
+            type: String,
+            default: "标题",
+        },
+        status: {
+            type: Number,
+            default: 0,
+        },
+    },
+    data() {
+        return {
+            statusLabel,
+        };
+    },
+};
+</script>
+
+<style scoped>
+#title {
+    color: var(--el-color-black);
+    text-align: left;
+    font-size: 16px;
+    font-weight: bold;
+    padding: 0;
+}
+#status {
+    color: var(--el-text-color-placeholder);
+    font-size: 12px;
+}
+.wrap {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+    padding: 15px;
+    box-sizing: border-box;
+
+    height: 100%;
+    width: 180px;
+
+    border: 1px solid var(--el-border-color);
+    border-radius: 15px;
+}
+
+.el-divider {
+    margin: 10px 0;
+}
+</style>
--- src/components/function/ModuleControl.vue
+++ src/components/function/ModuleControl.vue
@@ -1,13 +1,38 @@
 <template>
-    <FunctionLayout title="模块控制"> </FunctionLayout>
+    <FunctionLayout title="模块控制" style="width: 100%">
+        <!-- <div style="overflow-x: scroll; width: auto"> -->
+        <el-scrollbar style="s">
+            <el-space size="large">
+                <ModuleControlCell v-for="module in modules" :title="module"></ModuleControlCell>
+            </el-space>
+        </el-scrollbar>
+        <!-- </div> -->
+    </FunctionLayout>
 </template>
 
 <script lang="ts">
 import FunctionLayout from "../layout/FunctionLayout.vue";
+import ModuleControlCell from "../ModuleControlCell.vue";
 
 export default {
     components: {
         FunctionLayout,
+        ModuleControlCell,
+    },
+
+    data() {
+        return {
+            modules: ["处理核心", "网络爬虫", "人物渲染器"],
+        };
     },
 };
 </script>
+
+<style scoped>
+#modules-wrap {
+    display: flex;
+
+    height: 100%;
+    margin-bottom: 15px;
+}
+</style>

cf273bf68c1c188ba65f2e3a6f037e02fa8946ff
fucloud-macbook <270928474@qq.com>
2024-05-10 12:16:15 +0800

    feat: add Interact Page

--- src/views/sub/Interact.vue
+++ src/views/sub/Interact.vue
@@ -1,5 +1,27 @@
-<template></template>
+<template>
+    <div class="row-wrap">
+        <!-- Notice: 需要给两块地方单独设置一下占比 以解决宽度溢出的问题 -->
+        <TalkArea style="width: 50%; height: 100%"></TalkArea>
+    </div>
+</template>
 
 <script lang="ts">
-export default {};
+import TalkArea from "@/components/function/interact/TalkArea.vue";
+
+export default {
+    components: {
+        TalkArea,
+    },
+};
 </script>
+
+<style scoped>
+.col-wrap {
+    display: flex;
+    flex-direction: column;
+}
+.row-wrap {
+    display: flex;
+    height: 100%;
+}
+</style>

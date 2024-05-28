bbf113e0d68501cf15debec125813cfaf8b37def
fucloud-macbook <270928474@qq.com>
2024-05-28 14:31:20 +0800

    feat: EmptyPlaceholder

---
+++ src/components/EmptyPlaceholder.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="empty-wrap" :style="{ height: height + 'px' }">
+        <el-icon style="font-size: 24px; margin-bottom: 5px"><InfoFilled /></el-icon>
+        {{ text }}
+    </div>
+</template>
+
+<script>
+import { InfoFilled } from "@element-plus/icons-vue";
+export default {
+    components: {
+        InfoFilled,
+    },
+    props: {
+        text: String,
+        height: {
+            type: Number,
+            default: 150,
+        },
+    },
+};
+</script>
+
+<style scoped>
+.empty-wrap {
+    width: 100%;
+    height: 100%;
+    border-radius: 10px;
+    padding: 15px;
+    box-sizing: border-box;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    color: gray;
+    border: 1px solid var(--el-border-color);
+    background-color: var(--el-bg-color-page);
+}
+</style>
--- src/components/function/control/ModuleControl.vue
+++ src/components/function/control/ModuleControl.vue
@@ -6,19 +6,15 @@
                 <ModuleControlCell v-for="module in modules" :info="module"></ModuleControlCell>
             </el-space>
         </el-scrollbar>
-        <div v-else id="empty-wrap">
-            <el-icon style="font-size: 24px; margin-bottom: 5px"><InfoFilled /></el-icon>
-            未加载模块
-        </div>
+        <EmptyPlaceholder text="未加载模块" v-else></EmptyPlaceholder>
         <!-- </div> -->
     </FunctionLayout>
 </template>
 
 <script lang="ts">
-import { InfoFilled } from "@element-plus/icons-vue";
-
 import FunctionLayout from "@/components/layout/FunctionLayout.vue";
 import ModuleControlCell from "@/components/ModuleControlCell.vue";
+import EmptyPlaceholder from "@/components/EmptyPlaceholder.vue";
 
 import { useModuleStore } from "@/store";
 
@@ -28,7 +24,7 @@ export default {
             moduleStore: useModuleStore(),
         };
     },
-    components: { InfoFilled, FunctionLayout, ModuleControlCell },
+    components: { FunctionLayout, ModuleControlCell, EmptyPlaceholder },
     computed: {
         modules() {
             return this.moduleStore.controllableModuleList;
@@ -43,21 +39,5 @@ export default {
 
     height: 100%;
     margin-bottom: 15px;
-}
-#empty-wrap {
-    width: 100%;
-    height: 100%;
-    border-radius: 10px;
-    padding: 15px;
-    box-sizing: border-box;
-
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-
-    color: gray;
-    border: 1px solid var(--el-border-color);
-    background-color: var(--el-bg-color-page);
 }
 </style>

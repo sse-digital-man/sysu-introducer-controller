64a8b941b32e439f5cc76838ba4bfb8c96f09b3d
fucloud-macbook <270928474@qq.com>
2024-06-23 01:02:29 +0800

    import: vue3-tree-org

--- src/main.ts
+++ src/main.ts
@@ -8,5 +8,8 @@ import "./styles/element/index.scss";
 import router from "./router";
 import store from "./store";
 
+import vue3TreeOrg from "vue3-tree-org";
+import "vue3-tree-org/lib/vue3-tree-org.css";
+
 const app = createApp(App);
-app.use(ElementPlus).use(router).use(store).mount("#app");
+app.use(ElementPlus).use(router).use(store).use(vue3TreeOrg).mount("#app");
